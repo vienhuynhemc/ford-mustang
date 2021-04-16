52a33e2d6d7df9bb27cbd0609cf93525a5d25273
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-04-16 13:57:05 +0700

    ...

---
+++ src/app/chi-tiet-phan1/chi-tiet-phan1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chi-tiet-phan1',
+  templateUrl: './chi-tiet-phan1.component.html',
+  styleUrls: ['./chi-tiet-phan1.component.scss']
+})
+export class ChiTietPhan1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/chi-tiet-phan4/chi-tiet-phan4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chi-tiet-phan4',
+  templateUrl: './chi-tiet-phan4.component.html',
+  styleUrls: ['./chi-tiet-phan4.component.scss']
+})
+export class ChiTietPhan4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
