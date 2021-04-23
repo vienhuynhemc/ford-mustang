7b4b20c6240eb0cc781bf3a6b0fa8b4e8a8db7bc
dang3tion <65795540+dang3tion@users.noreply.github.com>
2021-04-23 15:05:32 +0700

    xong UI admin

---
+++ src/app/sua-thongtin/sua-thongtin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sua-thongtin',
+  templateUrl: './sua-thongtin.component.html',
+  styleUrls: ['./sua-thongtin.component.scss']
+})
+export class SuaThongtinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
